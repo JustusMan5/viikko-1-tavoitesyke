c62cd78c3dcf42b0f67b3fd4f85054337ae5f5ba
JustusMan5 <jho.honkanen@gmail.com>
2025-11-03 17:55:15 +0200

    Completed.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState<string>('');
+  const ageAsNumber : number = !isNaN(Number(age)) === true ? Number(age) : 0;
+  const lower: number = Math.round((220 - ageAsNumber) * 0.65);
+  const upper: number = Math.round((220 - ageAsNumber) * 0.85);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.heading}>Heart Rate Limits Calculator</Text>
+      <Text>Enter your age:</Text>
       <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        keyboardType="numeric"
+        placeholder="Age"
+        value={age}
+        onChangeText={text => setAge(text)}
+      />
+      <Text>Lower limit: {lower.toFixed(2)}</Text>
+      <Text>Upper limit: {upper.toFixed(2)}</Text>
     </View>
   );
 }
@@ -16,5 +32,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  text: {
+    marginVertical: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: 100,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
